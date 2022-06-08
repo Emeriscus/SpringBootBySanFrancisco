10c564bf55fa5964ecf7540ea672579b08f9d50a
Emeriscus <kimre88@gmail.com>
2022-06-08 20:04:48 +0200

    first commit

---
+++ src/main/java/springBootProject/SpringBootBySanFranciscoApplication.java
@@ -0,0 +1,13 @@
+package springBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBySanFranciscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBySanFranciscoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springBootProject/SpringBootBySanFranciscoApplicationTests.java
@@ -0,0 +1,13 @@
+package springBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBySanFranciscoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
